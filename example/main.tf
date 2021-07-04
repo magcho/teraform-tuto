15e282c1c02ea8447a371229f0f5beb79b4462a1
magcho <mail@magcho.com>
2021-07-04 20:07:26 +0900

    up

--- example/main.tf
+++ example/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "example_ec2" {
   }
 }
 resource "aws_instance" "example" {
-  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  ami                    = "ami-0c3fd0f5d33134a76"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
