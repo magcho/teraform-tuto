393b0abfd016ede4a7a6f99be81a9ca518da87d0
magcho <mail@magcho.com>
2021-07-05 23:10:28 +0900

    8.3

--- ws/alb/main.tf
+++ ws/alb/main.tf
@@ -70,8 +70,29 @@ resource "aws_lb_listener" "http" {
 }
 
 
+# 8.4
+data "aws_route53_zone" "example" {
+  name = "tf.magcho.com"
+}
+
+# 8.6
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name    = data.aws_route53_zone.example.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
 
 
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
+}
+output "domain_name" {
+  value = aws_route53_record.example.name
 }
