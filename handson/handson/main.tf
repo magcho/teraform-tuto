47499ad764c954a954e37b521c7eb60126c7e1cc
magcho <mail@magcho.com>
2021-07-07 19:22:01 +0900

    ecs

--- handson/handson/main.tf
+++ handson/handson/main.tf
@@ -237,3 +237,109 @@ resource "aws_alb_listener" "main" {
     }
   }
 }
+
+
+# ECS
+resource "aws_ecs_task_definition" "main" {
+  family                   = "handson"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu    = "256"
+  memory = "512"
+
+  network_mode          = "awsvpc"
+  container_definitions = <<EOL
+[
+  {
+    "name": "nginx",
+    "image": "nginx:1.14",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOL
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "handson"
+}
+
+resource "aws_lb_target_group" "main" {
+  name   = "handson"
+  vpc_id = aws_vpc.main.id
+
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+
+  health_check {
+    port = 80
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = aws_alb_listener.main.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
+resource "aws_security_group" "ecs" {
+  name        = "handson-ecs"
+  description = "handson-ecs"
+
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-ecs"
+  }
+}
+resource "aws_security_group_rule" "ecs" {
+  security_group_id = aws_security_group.ecs.id
+  type              = "ingress"
+
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = ["10.0.0.0/16"]
+}
+resource "aws_ecs_service" "main" {
+  name = "handson"
+  depends_on = [
+    aws_lb_listener_rule.main
+  ]
+  launch_type     = "FARGATE"
+  desired_count   = 1
+  task_definition = aws_ecs_task_definition.main.arn
+  cluster         = aws_ecs_cluster.main.id
+  network_configuration {
+    subnets         = [aws_subnet.private_1a.id, aws_subnet.private_1c.id, aws_subnet.private_1d.id]
+    security_groups = [aws_security_group.ecs.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "nginx"
+    container_port   = "80"
+  }
+}
