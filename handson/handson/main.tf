cbc5bfab3ea6c89c37ce46bd917bf47b02c8c2ca
magcho <mail@magcho.com>
2021-07-07 18:18:11 +0900

    nat

--- handson/handson/main.tf
+++ handson/handson/main.tf
@@ -129,6 +129,7 @@ resource "aws_route" "public" {
 resource "aws_route_table_association" "public_1a" {
   subnet_id      = aws_subnet.public_1a.id
   route_table_id = aws_route_table.public.id
+
 }
 resource "aws_route_table_association" "public_1c" {
   subnet_id      = aws_subnet.public_1c.id
@@ -137,4 +138,54 @@ resource "aws_route_table_association" "public_1c" {
 resource "aws_route_table_association" "public_1d" {
   subnet_id      = aws_subnet.public_1d.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_1a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+resource "aws_route_table" "private_1c" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+resource "aws_route_table" "private_1d" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "handson-private-1d"
+  }
+}
+
+resource "aws_route" "private_1a" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id = aws_route_table.private_1a.id
+  nat_gateway_id = aws_nat_gateway.nat_1a.id
+}
+resource "aws_route" "private_1c" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id = aws_route_table.private_1c.id
+  nat_gateway_id = aws_nat_gateway.nat_1c.id
+}
+resource "aws_route" "private_1d" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id = aws_route_table.private_1d.id
+  nat_gateway_id = aws_nat_gateway.nat_1d.id
+}
+resource "aws_route_table_association" "private_1a" {
+  subnet_id = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.private_1a.id
+}
+resource "aws_route_table_association" "private_1c" {
+  subnet_id = aws_subnet.private_1c.id
+  route_table_id = aws_route_table.private_1c.id
+}
+resource "aws_route_table_association" "private_1d" {
+  subnet_id = aws_subnet.private_1d.id
+  route_table_id = aws_route_table.private_1d.id
 }
