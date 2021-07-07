4e9b56c2b8205216a76aebd3c4f1aefaf6e90ac4
magcho <mail@magcho.com>
2021-07-07 17:56:25 +0900

    up

---
+++ handson/handson/main.tf
@@ -0,0 +1,114 @@
+
+
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "handson-public-1a"
+  }
+}
+resource "aws_subnet" "public_1c" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    Name = "handson-public-1c"
+  }
+}
+resource "aws_subnet" "public_1d" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.3.0/24"
+  tags = {
+    Name = "handson-public-1d"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.10.0/24"
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+resource "aws_subnet" "private_1c" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+resource "aws_subnet" "private_1d" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.30.0/24"
+  tags = {
+    Name = "handson-private-1d"
+  }
+}
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+
+resource "aws_eip" "nat_1a" {
+  vpc = true
+  tags = {
+    Name = "handson-natgw-1a"
+  }
+}
+resource "aws_nat_gateway" "nat_1a" {
+  subnet_id = aws_subnet.public_1a.id
+  allocation_id = aws_eip.nat_1a.id
+  tags = {
+    Name = "hadson-1a"
+  }
+}
+resource "aws_eip" "nat_1c" {
+  vpc = true
+  tags = {
+    Name = "handson-natgw-1c"
+  }
+}
+resource "aws_nat_gateway" "nat_1c" {
+  subnet_id = aws_subnet.public_1c.id
+  allocation_id = aws_eip.nat_1c.id
+  tags = {
+    Name = "hadson-1c"
+  }
+}
+resource "aws_eip" "nat_1d" {
+  vpc = true
+  tags = {
+    Name = "handson-natgw-1d"
+  }
+}
+resource "aws_nat_gateway" "nat_1d" {
+  subnet_id = aws_subnet.public_1d.id
+  allocation_id = aws_eip.nat_1d.id
+  tags = {
+    Name = "hadson-1d"
+  }
+}
