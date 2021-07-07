ece4d44fd261c8bf985b2e47e5f62874749fa9f7
magcho <mail@magcho.com>
2021-07-07 22:50:05 +0900

    https

--- handson/handson/main.tf
+++ handson/handson/main.tf
@@ -343,3 +343,99 @@ resource "aws_ecs_service" "main" {
     container_port   = "80"
   }
 }
+
+# HTTPS
+variable "domain" {
+  description = "my managed domain"
+  type        = string
+  default     = "tf.magcho.com"
+}
+
+data "aws_route53_zone" "main" {
+  name         = var.domain
+  private_zone = false
+}
+resource "aws_acm_certificate" "main" {
+  domain_name       = var.domain
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "validation" {
+
+  depends_on = [aws_acm_certificate.main]
+
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main.zone_id
+}
+resource "aws_acm_certificate_validation" "example" {
+  # SSL証明書の検証完了まで待機
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
+resource "aws_route53_record" "main" {
+  type    = "A"
+  name    = var.domain
+  zone_id = data.aws_route53_zone.main.id
+
+  alias {
+    name = aws_alb.main.dns_name
+    zone_id = aws_alb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_alb.main.arn
+  certificate_arn   = aws_acm_certificate.main.arn
+
+  port     = 443
+  protocol = "HTTPS"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+resource "aws_lb_listener_rule" "http_to_https" {
+  listener_arn = aws_alb_listener.main.arn
+
+  action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+  condition {
+    host_header {
+      values = [var.domain]
+    }
+  }
+}
+resource "aws_security_group_rule" "alb_https" {
+  security_group_id = aws_security_group.alb.id
+
+  type = "ingress"
+
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
